bb4d0fd3c77a3c6d984bfb814d06176208e546b9
Ale-zand <ale_zand@hotmail.com>
2023-05-23 14:16:29 -0300

    Servidor com MySQL e Sequelize. Implementacao do CRUD

---
+++ api/models/pessoas.js
@@ -0,0 +1,44 @@
+'use strict';
+const {
+  Model, DataTypes
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Pessoas extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Pessoas.init({
+    nome: DataTypes.STRING,
+    ativo: DataTypes.BOOLEAN,
+    email: DataTypes.STRING,
+    role: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Pessoas',
+  });
+  return Pessoas;
+};
+
+/* Modelo de código usado no curso da alura ao criar modelos pelo sequelize (versão antiga):
+
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Pessoas = sequelize.define('Pessoas', {
+    nome: DataTypes.STRING,
+    ativo: DataTypes.BOOLEAN,
+    email: DataTypes.STRING,
+    role: DataTypes.STRING
+  }, {});
+  Pessoas.associate = function(models) {
+    // assiociations can be defined here
+  };
+  return Pessoas;
+};
+
+*/
